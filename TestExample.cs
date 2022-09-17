87184709d9470b66daf1b822c4b694c81366a992
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-09-17 17:45:47 +0400

    Решил задачу

--- TestExample.cs
+++ TestExample.cs
@@ -12,9 +12,8 @@
 */
 
 Console.Write("Введите количество строк: ");
-int size = Convert.ToInt32(Console.ReadLine());
-string[] array1 = new string[size];
-string[] array2 = new string[size];
+int s = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[s];
 
 string[] CreateArray1(string[] args)
 {
@@ -23,63 +22,46 @@ string[] CreateArray1(string[] args)
             Console.WriteLine("Чтобы составить элемент массива, введите строку №{0}:", i + 1);
             array1[i] = Console.ReadLine();
         }
-        return array1;    
+        Console.WriteLine();
+        return array1;        
 }
-// Console.WriteLine();
 
-void PrintArray1(string[] array1)
+void PrintArray(string[] array)
 {
     Console.Write("Вывод массива: [");
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < array.Length; i++)
         {
-            if (i < array1.Length-1) Console.Write($"\"{array1[i]}\", ");
-            else Console.Write($"\"{array1[i]}\"");
+            if (i < array.Length-1) Console.Write($"\"{array[i]}\", ");
+            else Console.Write($"\"{array[i]}\"");
         }
     Console.Write("]");
+    Console.WriteLine();
 }
-// Console.WriteLine();
-
-// var newarray = array.Where(x => x.Length <= 3).ToArray();
-/* void ResultArray(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
-}*/
 
 string[] CreateArray2(string[] args)
 {
+    int size = 0;
     int length = 3;
-    int element = 0;
-
-    for (int j = 0; j < size; j++)
+    for (int i = 0; i < args.Length; i++)
     {
-        if (array1[j].Length <= length)
+        if(args[i].Length <= length) size++;
+    }
+
+    string[] array = new string[size];
+    int j = 0;
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        if(args[i].Length <= length)
         {
-            array2[element] = array1[j];
-            element++;
+            array[j] = args[i];
+            j++;
         }
     }
-    return array2;   
+    return array;   
 }
 
-void PrintArray2(string[] array2)
-{
-    Console.Write("Нужный массив: [");
-    for (int i = 0; i < array2.Length; i++)
-    { 
-        if (i < array2.Length-1) Console.Write($"\"{array2[i]}\", ");
-        else Console.Write($"{array2[i]}");                                                  
-    }
-    Console.Write("]");
-}
 CreateArray1(array1);
-PrintArray1(array1);
-CreateArray2(array2);
-PrintArray2(array2);
+PrintArray(array1);
+string[] array2 = CreateArray2(array1);
+PrintArray(array2);
